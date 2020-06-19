3cb9edbe2d15e714bd86560049c7046d623aa67d
Onofer Dávid <david.onofer@icellmobilsoft.hu>
2020-06-19 14:55:13 +0200

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueCompositionApi from "@vue/composition-api";
+import Buefy from "buefy";
+import "./assets/scss/app.scss";
+
+Vue.use(Buefy);
+Vue.use(VueCompositionApi);
 
 Vue.config.productionTip = false;
 
