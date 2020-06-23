21e74e646d7cd845826924524ead45afee5f96ae
Onofer Dávid <david.onofer@icellmobilsoft.hu>
2020-06-23 15:22:54 +0200

    POC - initial decorators

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -11,6 +17,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-implicit-any': 'off',
-    'no-explicit-any': 'off'
+    'no-explicit-any': 'off',
+    '@typescript-eslint/no-implicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 };
