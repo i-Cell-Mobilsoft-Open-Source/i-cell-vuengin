cba85fe947da0a4fd2c0aa92e5f988f33bc122af
Zrínyi András <andras.zrinyi@icellmobilsoft.hu>
2020-08-17 15:15:05 +0200

    HUGODEV-12218: vue-input component

--- src/views/Input/input-sandbox.vue
+++ src/views/Input/input-sandbox.vue
@@ -5,7 +5,13 @@
         <div class="box">
           <div class="padding-content">
             <div class="title">Input</div>
-            <v-icell-input :name="'userName'" :placeHolder="'Felhasználó neve'" :value="'Jóska'"></v-icell-input>
+            <v-icell-input
+              :type="'text'"
+              :label="'Falhasználó neve'"
+              :name="'userName'"
+              :placeHolder="'Felhasználó neve'"
+              :value="'Jóska'"
+            ></v-icell-input>
           </div>
           <v-collapse
             class="collapse-container"
@@ -13,7 +19,22 @@
             :label="'show code'"
             :hideLabel="'hide source'"
             :contentId="'basic-table'"
-            :content="code"
+            :content="code_SampleA"
+          ></v-collapse>
+        </div>
+
+        <div class="box">
+          <div class="padding-content">
+            <div class="title">Input - Password</div>
+            <v-icell-input :type="'password'" :label="'Jelszó'" :name="'password'" :placeHolder="'jelszó'"></v-icell-input>
+          </div>
+          <v-collapse
+            class="collapse-container"
+            :classes="'is-small is-light'"
+            :label="'show code'"
+            :hideLabel="'hide source'"
+            :contentId="'basic-table'"
+            :content="code_SampleB"
           ></v-collapse>
         </div>
       </div>
@@ -25,18 +46,34 @@
 import { ref } from '@vue/composition-api';
 
 export default {
+  name: 'InputComomentDemo',
   setup() {
-    const code = ref(
+    const code_SampleA = ref(
       '<template>\n' +
         '  <v-icell-input\n' +
+        '    :type="text"\n' +
+        '    :label="Falhasználó neve"\n' +
         '    :name="userName"\n' +
         '    :placeHolder="Felhasználó neve">\n' +
         '    :value="Jóska"\n' +
         '  </v-icell-input>\n' +
         '</template>'
     );
+
+    const code_SampleB = ref(
+      '<template>\n' +
+        '  <v-icell-input\n' +
+        '    :type="password"\n' +
+        '    :label="Jelszó"\n' +
+        '    :name="password"\n' +
+        '    :placeHolder="jelszó">\n' +
+        '  </v-icell-input>\n' +
+        '</template>'
+    );
+
     return {
-      code
+      code_SampleA,
+      code_SampleB
     };
   }
 };
