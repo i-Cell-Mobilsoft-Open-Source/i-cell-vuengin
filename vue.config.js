92e31a10cc197be4eb09ad60997a091b463ef4b1
Martus Gábor <gabesztx@gmail.com>
2020-08-28 23:36:31 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  /*  devServer: {
-    hot: true,
-    inline: true,
-  },*/
-  pluginOptions: {
-    browserSync: {
-      /*ui: {
-        port: 8080,
-      }*/
-    },
-  },
-};
+module.exports = {};
