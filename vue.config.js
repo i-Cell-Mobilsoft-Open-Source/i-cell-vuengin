3b33e509a31d32916447fa9951435066c907f379
Martus Gábor <gabesztx@gmail.com>
2020-08-28 18:36:56 +0200

    browserSync init

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /*  devServer: {
+    hot: true,
+    inline: true,
+  },*/
+  pluginOptions: {
+    browserSync: {
+      /*ui: {
+        port: 8080,
+      }*/
+    },
+  },
+};
